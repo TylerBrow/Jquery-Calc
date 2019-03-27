5bd7ccdaf7f528b79bcd91defcc7a4172fd68d09
Tyler Brow <tymuska@yahoo.com>
2019-03-27 14:24:53 -0700

    explore calc

---
+++ main.js
@@ -0,0 +1,44 @@
+$(function(){
+
+$('#add').on('click', function(e){
+    e.preventDefault()
+
+    var num1 = Number($('#num1').val())
+    var num2 = Number($('#num2').val())
+
+    $('#answer').html(num1 + num2)
+})
+
+$('#subtract').on('click', function(e){
+    e.preventDefault()
+
+    var num1 = Number($('#num1').val())
+    var num2 = Number($('#num2').val())
+
+    $('#answer').html(num1 - num2)
+})
+
+$('#multiply').on('click', function(e){
+    e.preventDefault()
+
+    var num1 = Number($('#num1').val())
+    var num2 = Number($('#num2').val())
+
+    $('#answer').html(num1 * num2)
+})
+
+$('#divide').on('click', function(e){
+    e.preventDefault()
+
+    var num1 = Number($('#num1').val())
+    var num2 = Number($('#num2').val())
+
+    $('#answer').html(num1 / num2)
+})
+
+
+
+
+
+
+})
